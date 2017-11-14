afb82613e83a80253c4a7df2f166433be8db8939
Kolby Lacy <kolby.lacy@bison.howard.edu>
2017-11-13 23:20:49 -0500

    fixed the individual digit checking/conversion

--- helloWorld.asm
+++ helloWorld.asm
@@ -27,7 +27,6 @@ main:
 	la $a0, endline	
 	syscall
 
-	
 	la $t0, in_string
 
 hextoDec:
@@ -36,29 +35,95 @@ hextoDec:
 	lb $t1, 0($t0)
 
 
-	li $t2, 0x30
-	li $t3, 0x39
+	beq $t1, 0, convert
+	beq $t1, 10, convert
 
-	andi $t2, $t2, 0x000000ff #makes into a word for comparison
-	andi $t3, $t3, 0x000000ff
+	slt $s0, $t1, 48 #check if less than 0
+	sgt $s1, $t1, 57 #check if greater than 9
 
-	bltu $t1, $t2, ERROR #error if lower than 0x30 (ascii) 
-	bgt $t1, $t3, checkHex
+	beq $s0, 1, ERROR #Less than 1 means not hex
+	beq $s1, 1, checkCHex #checks if the number is a-f or A-F
 
-	j isDigit
-
-	
-
-
-
-isDigit:
 
 	#since character is between 0 and 9
-	subu $t1, $t1, 0x30
-	j finito
+	addi $t1, $t1, -30
 
 
-checkHex:
+	j incrementChar
+
+
+#list of registers used for each index |0|1|2|3|4|5|6|7| is |$t3|$t4|$t5|$t6|$t7|$t8|$t9|$s2|
+
+
+checkCHex:
+	
+	slt $s0, $t1, 65 #check if less than A
+	sgt $s1, $t1, 70 #check if greater than F
+
+	beq $s0, 1, ERROR #Not a hex digit
+	beq $s1, 1, checkLHex #checks if the number is a-f
+
+	addi $t1, $t1, -55
+
+	j incrementChar
+
+
+checkLHex:
+
+	slt $s0, $t1, 97 #check if less than a
+	sgt $s1, $t1, 102 #check if greater than f
+
+	beq $s0, 1, ERROR #Not a hex digit
+	beq $s1, 1, ERROR #not a hex digit
+
+	addi $t1, $t1, -87
+	j incrementChar
+
+incrementChar:
+
+	addi $t2, $t2, 1 #increments the counter, which is $t2
+	beq $t2, 1, addReg1
+	beq $t2, 2, addReg2
+	beq $t2, 3, addReg3
+	beq $t2, 4, addReg4
+	beq $t2, 5, addReg5
+	beq $t2, 6, addReg6
+	beq $t2, 7, addReg7
+	beq $t2, 8, addReg8
+	addi $t0, $t0, 1 #moves up the address to a new character
+	j hextoDec
+
+addReg1:
+	addi $t3, $t1
+	j hextoDec
+
+addReg2:
+	addi $t4, $t1
+	j hextoDec
+
+addReg3:
+	addi $t5, $t1
+	j hextoDec
+
+addReg4:
+	addi $t6, $t1
+	j hextoDec
+
+addReg5:
+	addi $t7, $t1
+	j hextoDec
+
+addReg6:
+	addi $t8, $t1
+	j hextoDec
+
+addReg7:
+	addi $t9, $t1
+	j hextoDec
+
+addReg8:
+	addi $s2, $t1
+	j hextoDec
 
 ERROR:
 
@@ -66,8 +131,13 @@ ERROR:
 	la $a0, errorPrompt
 	syscall
 
+convert:
+	beq $t2, 0, ERROR
+	bgt $t2, 8, ERROR
 
-finito:
+	
+
+endProc:
 	#endline
 	li $v0, 4
 	la $a0, endline	
@@ -75,7 +145,7 @@ finito:
 
 	#output the new string
 	li $v0, 4
-	lb $a0, 1($t1)
+	la $a0, ($t0)
 	syscall
 
 	li $v0, 10
